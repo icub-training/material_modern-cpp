e3c7a0659b45a9d5b8cc537c4730fa9891935fef
marco.a.accame <marco.accame@iit.it>
2023-05-29 18:34:00 +0200

    added framework for exercises day03

---
+++ exercises/day03/mcore01/include/mcore01.h
@@ -0,0 +1,30 @@
+
+/*
+ * Copyright (C) 2023 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+
+// - include guard ----------------------------------------------------------------------------------------------------
+
+#ifndef __MCORE01_H_
+#define __MCORE01_H_
+
+
+// -------------------------------------------------------------------------------------------------------------------
+// MODERNC++CORE-nullptr-api
+// 
+
+namespace mcore01 { namespace ex01 {
+    void run();
+}}
+
+// others in here
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
---
+++ exercises/day03/mcore01/src/main.cpp
@@ -0,0 +1,22 @@
+
+/*
+ * Copyright (C) 2023 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+
+
+#include<iostream>
+
+#include "mcore01.h"
+
+int main(int argc, char *argv[]) 
+{
+   std::cout << "Hello World! of Modern C++" << std::endl;
+
+   // nullptr 
+   mcore01::ex01::run();
+   
+   return 0;
+}
